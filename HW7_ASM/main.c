fc4d45d2db91546dd3c6ec6701243acc1afec912
Pakulina <pakulina97@mail.ru>
2023-03-05 20:18:26 +0500

    Add HW_7

---
+++ HW7_ASM/main.c
@@ -0,0 +1,99 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAIN_TYPE int64_t
+#define EMPTY_STR ("")
+
+MAIN_TYPE data[] = {4, 8, 15, 16, 23, 42};
+MAIN_TYPE data_length = sizeof(data) / sizeof(data[0]);
+
+void print_int(MAIN_TYPE *v) {
+	fprintf(stdout, "%ld ", *v);
+	fflush(stdout);
+}
+
+MAIN_TYPE p(MAIN_TYPE v) {
+	return v & 1;
+}
+
+// Можно сделать через указатели
+MAIN_TYPE *add_element(MAIN_TYPE *prev, MAIN_TYPE value) {
+	MAIN_TYPE *ret = (MAIN_TYPE *)malloc(2 * sizeof(MAIN_TYPE));
+	if (!ret) {
+		return NULL;
+	}
+	ret[1] = prev ? prev : NULL;
+	ret[0] = value;
+
+	return ret;
+}
+
+// Aналог функции std::for_each
+void m(MAIN_TYPE *p, void (*func)(MAIN_TYPE *)) {
+	if (!p) {
+		return;
+	}
+
+	MAIN_TYPE *prev = (void *)(p[1]);
+	func(p);
+	m(prev, func);
+}
+
+// Aналог функции std::copy_if
+MAIN_TYPE *f(MAIN_TYPE *p, MAIN_TYPE *op, MAIN_TYPE (*func)(MAIN_TYPE)) {
+	if (!p) {
+		return op;
+	}
+	if (func(p[0])) {
+		op = add_element(op, p[0]);
+	}
+	return f((MAIN_TYPE*) p[1], op, func);
+}
+
+void free_p(MAIN_TYPE *p) {
+	free(p);
+}
+
+int main() {
+	/*
+	 * Оригинал, сборка:
+	 * nasm -f elf64 file.asm
+	 * gcc -no-pie -o file file.o
+	 *
+	 * Вывод:
+	 * 4 8 15 16 23 42
+		 23 15
+	 */
+
+	MAIN_TYPE *s1 = NULL;
+	MAIN_TYPE *s2 = NULL;
+	MAIN_TYPE len = data_length;
+
+	// Действия с односвязным списком
+
+	while (len) {
+		len = len - 1;
+		s1 = add_element(s1, data[len]);
+	}
+
+	m(s1, print_int);
+	puts(EMPTY_STR);
+
+	s2 = f(s1, s2, p); // S2 от s1 по р(...)
+
+	m(s2, print_int);
+	puts(EMPTY_STR);
+
+	m(s1, free_p); // Этого должно быть достаточно
+	m(s2, free_p);
+
+	// В ячейкай хранится объект из 2х элементов, но обьектов N кол-во,
+	// удаление будет N раз
+
+	// Output copy:
+	// 4 8 15 16 23 42
+	// 23 15
+
+	return 0;
+}
---
+++ HW7_ASM/original.asm
@@ -0,0 +1,153 @@
+    bits 64
+    extern malloc, puts, printf, fflush, abort
+    global main
+
+    section   .data
+empty_str: db 0x0
+int_format: db "%ld ", 0x0
+data: dq 4, 8, 15, 16, 23, 42
+data_length: equ ($-data) / 8
+
+    section   .text
+;;; print_int proc
+print_int:
+    mov rsi, rdi
+    mov rdi, int_format
+    xor rax, rax
+    call printf
+
+    xor rdi, rdi
+    call fflush
+
+    ret
+
+;;; p proc
+p:
+    mov rax, rdi
+    and rax, 1
+    ret
+
+;;; add_element proc
+add_element:
+    push rbp
+    push rbx
+
+    mov rbp, rdi
+    mov rbx, rsi
+
+    mov rdi, 16
+    call malloc
+    test rax, rax
+    jz abort
+
+    mov [rax], rbp
+    mov [rax + 8], rbx
+
+    pop rbx
+    pop rbp
+
+    ret
+
+;;; m proc
+m:
+    test rdi, rdi
+    jz outm
+
+    push rbp
+    push rbx
+
+    mov rbx, rdi
+    mov rbp, rsi
+
+    mov rdi, [rdi]
+    call rsi
+
+    mov rdi, [rbx + 8]
+    mov rsi, rbp
+    call m
+
+    pop rbx
+    pop rbp
+
+outm:
+    ret
+
+;;; f proc
+f:
+    mov rax, rsi
+
+    test rdi, rdi
+    jz outf
+
+    push rbx
+    push r12
+    push r13
+
+    mov rbx, rdi
+    mov r12, rsi
+    mov r13, rdx
+
+    mov rdi, [rdi]
+    call rdx
+    test rax, rax
+    jz z
+
+    mov rdi, [rbx]
+    mov rsi, r12
+    call add_element
+    mov rsi, rax
+    jmp ff
+
+z:
+    mov rsi, r12
+
+ff:
+    mov rdi, [rbx + 8]
+    mov rdx, r13
+    call f
+
+    pop r13
+    pop r12
+    pop rbx
+
+outf:
+    ret
+
+;;; main proc
+main:
+    push rbx
+
+    xor rax, rax
+    mov rbx, data_length
+adding_loop:
+    mov rdi, [data - 8 + rbx * 8]
+    mov rsi, rax
+    call add_element
+    dec rbx
+    jnz adding_loop
+
+    mov rbx, rax
+
+    mov rdi, rax
+    mov rsi, print_int
+    call m
+
+    mov rdi, empty_str
+    call puts
+
+    mov rdx, p
+    xor rsi, rsi
+    mov rdi, rbx
+    call f
+
+    mov rdi, rax
+    mov rsi, print_int
+    call m
+
+    mov rdi, empty_str
+    call puts
+
+    pop rbx
+
+    xor rax, rax
+    ret
